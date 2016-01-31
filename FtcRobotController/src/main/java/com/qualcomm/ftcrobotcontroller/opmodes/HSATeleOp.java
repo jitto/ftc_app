f2e3dda1f5fba5534d407585b25b9f2d22391140
rxg823 <justine.thomas@capitalone.com>
2016-01-30 18:27:55 -0600

    Update servo controls

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/HSATeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/HSATeleOp.java
@@ -6,19 +6,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 public class HSATeleOp extends OpMode {
-
-    final static double BASKET_MIN_RANGE  = 0.10;
-    final static double BASKET_MAX_RANGE  = 0.9;
-
-    double servoDelta = 0.008;
-
     DcMotor motorRight;
     DcMotor motorLeft;
     DcMotor motorWinch;
     Servo winchAngle;
+    double winchAnglePosition = 0.7;
     Servo armDirection;
+    double armDirectionPosition = 0.91;
     Servo elbow;
+    double elbowPosition = 0.19;
     Servo basket;
+    double basketPositon = 0;
+
 
     @Override
     public void init() {
@@ -30,6 +29,11 @@ public class HSATeleOp extends OpMode {
         armDirection = hardwareMap.servo.get("armDirection");
         elbow = hardwareMap.servo.get("elbow");
         basket = hardwareMap.servo.get("bucket");
+
+        winchAngle.setPosition(winchAnglePosition);
+        armDirection.setPosition(armDirectionPosition);
+        elbow.setPosition(elbowPosition);
+        basket.setPosition(basketPositon);
     }
 
     @Override
@@ -46,23 +50,29 @@ public class HSATeleOp extends OpMode {
 
         motorWinch.setPower(gamepad1.right_stick_y / 4);
 
-        updateServoPosition(winchAngle, gamepad1.a, gamepad2.b, BASKET_MIN_RANGE, BASKET_MAX_RANGE);
-        updateServoPosition(armDirection, gamepad2.dpad_left, gamepad2.dpad_right, BASKET_MIN_RANGE, BASKET_MAX_RANGE);
-        updateServoPosition(elbow, gamepad2.dpad_up, gamepad2.dpad_down, BASKET_MIN_RANGE, BASKET_MAX_RANGE);
-        updateServoPosition(basket, gamepad2.a, gamepad2.b, BASKET_MIN_RANGE, BASKET_MAX_RANGE);
+        winchAnglePosition = updateServoPosition(winchAnglePosition, gamepad2.dpad_up, gamepad2.dpad_down, 0.001, 0, 1);
+        winchAngle.setPosition(winchAnglePosition);
+        armDirectionPosition = updateServoPosition(armDirectionPosition, gamepad2.dpad_right, gamepad2.dpad_left, 0.002, 0, 1);
+        armDirection.setPosition(armDirectionPosition);
+        elbowPosition = updateServoPosition(elbowPosition, gamepad2.x, gamepad2.y, 0.002, 0, 1);
+        elbow.setPosition(elbowPosition);
+        basketPositon = updateServoPosition(basketPositon, gamepad2.a, gamepad2.b, 0.01, 0, 1);
+        basket.setPosition(basketPositon);
 
-        telemetry.addData("x " + String.format("%.2f", gamepad1.left_stick_x) + " y ", String.format("%.2f", gamepad1.left_stick_y));
-        telemetry.addData("left " + String.format("%.2f", left) + " right ", String.format("%.2f", right));
+        telemetry.addData("winchAngle " + String.format("%.2f", winchAngle.getPosition()), " armDirection" + String.format("%.2f", armDirection.getPosition()));
+        telemetry.addData("elbow " + String.format("%.2f", elbow.getPosition()), " basket" + String.format("%.2f", basket.getPosition()));
+
+//        telemetry.addData("x " + String.format("%.2f", gamepad1.left_stick_x) + " y ", String.format("%.2f", gamepad1.left_stick_y));
+//        telemetry.addData("left " + String.format("%.2f", left) + " right ", String.format("%.2f", right));
 //        telemetry.addData("Current shoulder " + motorShoulder.getCurrentPosition() + " elbow ", motorElbow.getCurrentPosition());
 //        telemetry.addData("Target shoulder " + motorShoulder.getTargetPosition() + " elbow ", motorElbow.getTargetPosition());
 //        telemetry.addData("basket ", String.format("%.2f", basketPosition));
 //        telemetry.addData("motorShoulderPower ", String.format("%.2f", motorShoulder.getPower()));
     }
 
-    private void updateServoPosition(Servo servo, boolean incrementButton, boolean decrementButton, double minRange, double maxRange) {
-        double currentValue = servo.getPosition();
-        double basketMove = incrementButton ? servoDelta : decrementButton ? -servoDelta : 0;
-        servo.setPosition(Range.clip(currentValue + basketMove, minRange, maxRange));
+    private double updateServoPosition(double currentValue, boolean incrementButton, boolean decrementButton, double delta, double minRange, double maxRange) {
+        double basketMove = incrementButton ? delta : decrementButton ? -delta : 0;
+        return Range.clip(currentValue + basketMove, minRange, maxRange);
     }
 
     private double powerInRange(float power, float powerRange) {
