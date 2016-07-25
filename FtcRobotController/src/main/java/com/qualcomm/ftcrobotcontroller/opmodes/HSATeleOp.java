531749b5d8e5b59a911fb0373b41af3952a96def
rxg823 <justine.thomas@capitalone.com>
2016-07-25 07:40:06 -0500

    Add servo disable

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/HSATeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/HSATeleOp.java
@@ -3,37 +3,23 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.util.Range;
 
 public class HSATeleOp extends OpMode {
     DcMotor motorRight;
     DcMotor motorLeft;
-    DcMotor motorWinch;
-    Servo winchAngle;
-    double winchAnglePosition = 0.7;
-    Servo armDirection;
-    double armDirectionPosition = 0.91;
     Servo elbow;
-    double elbowPosition = 0.19;
-    Servo basket;
-    double basketPositon = 0;
-
+    double elbowPosition = 0.00;
+    boolean servoOn = false;
 
     @Override
     public void init() {
         motorRight = hardwareMap.dcMotor.get("right");
         motorLeft = hardwareMap.dcMotor.get("left");
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
-        motorWinch = hardwareMap.dcMotor.get("winch");
-        winchAngle = hardwareMap.servo.get("winchAngle");
-        armDirection = hardwareMap.servo.get("armDirection");
         elbow = hardwareMap.servo.get("elbow");
-        basket = hardwareMap.servo.get("bucket");
-
-        winchAngle.setPosition(winchAnglePosition);
-        armDirection.setPosition(armDirectionPosition);
         elbow.setPosition(elbowPosition);
-        basket.setPosition(basketPositon);
     }
 
     @Override
@@ -48,26 +34,18 @@ public class HSATeleOp extends OpMode {
         motorRight.setPower(right);
         motorLeft.setPower(left);
 
-        motorWinch.setPower(gamepad1.right_stick_y / 4);
+        servoOn = !gamepad2.x && (gamepad2.a || servoOn);
 
-        winchAnglePosition = updateServoPosition(winchAnglePosition, gamepad2.dpad_up, gamepad2.dpad_down, 0.001, 0, 1);
-        winchAngle.setPosition(winchAnglePosition);
-        armDirectionPosition = updateServoPosition(armDirectionPosition, gamepad2.dpad_right, gamepad2.dpad_left, 0.002, 0, 1);
-        armDirection.setPosition(armDirectionPosition);
-        elbowPosition = updateServoPosition(elbowPosition, gamepad2.x, gamepad2.y, 0.002, 0, 1);
-        elbow.setPosition(elbowPosition);
-        basketPositon = updateServoPosition(basketPositon, gamepad2.a, gamepad2.b, 0.01, 0, 1);
-        basket.setPosition(basketPositon);
+        if (servoOn) {
+            elbowPosition = updateServoPosition(elbowPosition, gamepad2.right_stick_y < -0.1, gamepad2.right_stick_y > 0.1, Math.abs(gamepad2.right_stick_y) / 200, 0, 1);
+            elbow.getController().pwmEnable();
+            elbow.setPosition(elbowPosition);
+        } else {
+            elbow.getController().pwmDisable();
+        }
 
-        telemetry.addData("winchAngle " + String.format("%.2f", winchAngle.getPosition()), " armDirection" + String.format("%.2f", armDirection.getPosition()));
-        telemetry.addData("elbow " + String.format("%.2f", elbow.getPosition()), " basket" + String.format("%.2f", basket.getPosition()));
-
-//        telemetry.addData("x " + String.format("%.2f", gamepad1.left_stick_x) + " y ", String.format("%.2f", gamepad1.left_stick_y));
-//        telemetry.addData("left " + String.format("%.2f", left) + " right ", String.format("%.2f", right));
-//        telemetry.addData("Current shoulder " + motorShoulder.getCurrentPosition() + " elbow ", motorElbow.getCurrentPosition());
-//        telemetry.addData("Target shoulder " + motorShoulder.getTargetPosition() + " elbow ", motorElbow.getTargetPosition());
-//        telemetry.addData("basket ", String.format("%.2f", basketPosition));
-//        telemetry.addData("motorShoulderPower ", String.format("%.2f", motorShoulder.getPower()));
+        telemetry.addData("elbow ", String.format("%.2f", elbow.getPosition()));
+        telemetry.addData("test ", elbow.getController().getPwmStatus());
     }
 
     private double updateServoPosition(double currentValue, boolean incrementButton, boolean decrementButton, double delta, double minRange, double maxRange) {
